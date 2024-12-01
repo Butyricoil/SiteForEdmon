c4d9e7c8e71e058bb038bab052513a61d47b5f60
Butyricoil <butyricoil@gmail.com>
2024-12-01 17:18:44 +0400

    add template

--- script.js
+++
@@ -1,9 +0,0 @@
-// Слайдер смены фона
-const backgroundImages = document.querySelectorAll('.background-image');
-let currentImageIndex = 0;
-
-setInterval(() => {
-    backgroundImages[currentImageIndex].classList.remove('active');
-    currentImageIndex = (currentImageIndex + 1) % backgroundImages.length;
-    backgroundImages[currentImageIndex].classList.add('active');
-}, 5000);
