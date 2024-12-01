cc4cf60eb82792afdaf698428893e5535bb71333
Butyricoil <butyricoil@gmail.com>
2024-12-01 21:35:37 +0400

    add logo + tel

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Инициализация Swiper с параметрами
+const swiper = new Swiper('.swiper-container', {
+    loop: true, // Зацикливаем слайдер
+    autoplay: {
+      delay: 5000, // Интервал между слайдами (5 секунд)
+    },
+    navigation: {
+      nextEl: '.swiper-button-next', // Кнопка для перехода к следующему слайду
+      prevEl: '.swiper-button-prev', // Кнопка для перехода к предыдущему слайду
+    },
+    pagination: {
+      el: '.swiper-pagination', // Пагинация
+      clickable: true, // Возможность кликать по пагинации
+    },
+    effect: 'fade', // Плавный эффект перехода между слайдами
+  });
+  
